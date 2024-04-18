42cb4fbb15743325d9fd0fe09ea6b50fbd3f2082
Krzysztof Kozminski <krzysztof.kozminski4@gmail.com>
2024-04-18 17:54:19 +0200

    CRUD operations on posts

--- MegaProjekt/Controllers/v1/TimeLineController.cs
+++ MegaProjekt/Controllers/v1/TimeLineController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 using MegaProjekt.Core.DTO;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 namespace MegaProjekt.WebAPI.Controllers.v1
 {
@@ -17,9 +18,9 @@ namespace MegaProjekt.WebAPI.Controllers.v1
             _dbcontext = dbContext;
         }
 
-        //do przekminy jak to zrobimy
-        [HttpPost("addPost")]
-        public async Task<IActionResult> DodajPost([FromForm] PostDTO request)
+        //do przekminy jak to zrobimy, na razie baza wszytskich funkcjonalności gotowa
+        [HttpPost("add")]
+        public async Task<IActionResult> AddPost([FromBody] PostDTO request)
         {
             if (!ModelState.IsValid)
             {
@@ -38,6 +39,69 @@ namespace MegaProjekt.WebAPI.Controllers.v1
 
             return Ok(nowyPost); 
         }
+
+        [HttpPut("edit/{id}")]
+        public async Task<IActionResult> EditPost(int id, [FromBody] PostDTO request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var post = await _dbcontext.PostDTOs.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            post.Photo = request.Photo;
+            post.Description = request.Description;
+
+            _dbcontext.Update(post);
+            await _dbcontext.SaveChangesAsync();
+
+            return Ok(post);
+        }
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeletePost(int id)
+        {
+            var post = await _dbcontext.PostDTOs.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            _dbcontext.PostDTOs.Remove(post);
+            await _dbcontext.SaveChangesAsync();
+
+            return Ok(post);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPost(int id)
+        {
+            var post = await _dbcontext.PostDTOs.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(post);
+        }
+
+        // GET api/v1/timeLine
+        [HttpGet]
+        public async Task<IActionResult> GetPosts()
+        {
+            var posts = await _dbcontext.PostDTOs.ToListAsync();
+
+            return Ok(posts);
+        }
+
     }
 }
 
